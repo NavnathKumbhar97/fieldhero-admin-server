0607e696eec62409de5b2f3190e028a93cafe6fc
NavnathKumbhar97 <nkumbhar@apexaglobal.com>
2023-01-18 19:39:43 +0530

    add agent master documetns

--- src/router/upload.ts
+++ src/router/upload.ts
@@ -50,12 +50,76 @@ const upload = multer({ storage: storage, fileFilter: fileFilter })
 
 //Upload route
 
+const storageFile = multer.diskStorage({
+    destination: (req: Request, file: Express.Multer.File, cb) => {
+        const p = `public/uploads/candidates/${req.params.id}/Agent_Master`
+        // `.../public/${req.params.id}/profile_image`
+        if (!fs.existsSync(p)) {
+            fs.mkdirSync(p, { recursive: true })
+        }
+        cb(null, p)
+    },
+    filename: (req: Request, file: Express.Multer.File, cb) => {
+        const datetimestamp = Date.now()
+        const newFilename = `${file.fieldname}-${
+            req.params.id
+        }-${datetimestamp}${path.extname(file.originalname)}`
+        cb(null, newFilename)
+    },
+    // destination: function (req, file, cb) {
+    //     cb(null, '../assets');
+    //  },
+    //  filename: function (req, file, cb) {
+    //     cb(null, Date.now() + '-' + file.originalname);
+    //  }
+})
+const fileFilterFile = (req: Request, file: Express.Multer.File, cb: any) => {
+    // if (
+    //     file.mimetype == "file"
+    // ) {
+        return cb(null, true)
+    // } else {
+    //     return cb(new Error("Only image files are allowed!"), false)
+    // }
+}
+
+
+const uploadFile = multer({ storage: storageFile, fileFilter: fileFilterFile })
+
 UploadRouter.post(
     "/upload-profile/:id",
     middleware.permission(
         helper.permissions.candidate_basic_upload_profile_image
     ),
     upload.single("image"),
+    async (req: Request, res: Response) => {
+        try {
+            const file = req.file
+            if (!file)
+                return res.status(httpStatus.Not_Found).json({
+                    code: httpStatus.Not_Found,
+                    message: "File not found",
+                    data: null,
+                })
+
+            const path = file.destination + "/" + file.filename
+            const results = await handler.UploadImage.UploadImgCandidate(
+            path)
+            // const { code, data, message } = results
+            res.send(results)
+        } catch (error:any) {
+            handler.express.handleRouterError(res, error)
+            console.log("got an error ",error);
+            res.send(error)
+        }
+    }
+)
+UploadRouter.post(
+    "/upload-agent-master/:id",
+    middleware.permission(
+        helper.permissions.candidate_basic_upload_profile_image
+    ),
+    uploadFile.single("file"),
     async (req: Request, res: Response) => {
         try {
             const file = req.file
